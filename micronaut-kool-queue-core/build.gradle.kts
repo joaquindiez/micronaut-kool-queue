60d6f2e96197ed2e0947692a64a94058bd3e6063
Joaquin Diez <joaquindiez@gmail.com>
2024-10-17 12:55:19 +0200

    refactor proyects for first commit

---
+++ micronaut-kool-queue-core/build.gradle.kts
@@ -0,0 +1,126 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.23"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.23"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.9.23"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.library") version "4.4.0"
+    id("io.micronaut.aot") version "4.4.0"
+    id("maven-publish")
+}
+
+version = "0.1.2"
+group = "joaquindiez"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    ksp("io.micronaut:micronaut-http-validation")
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+
+    implementation("io.micronaut.data:micronaut-data-jpa")
+    implementation("io.micronaut.data:micronaut-data-jdbc")
+    implementation("javax.persistence:javax.persistence-api") // API de JPA
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+
+
+    implementation("com.github.f4b6a3:uuid-creator:5.3.7")
+
+
+    compileOnly("io.micronaut:micronaut-http-client")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("io.micronaut:micronaut-http-client")
+}
+
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+}
+
+
+graalvmNative.toolchainDetection = false
+micronaut {
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("freesoullabs.*")
+    }
+    aot {
+    // Please review carefully the optimizations enabled below
+    // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading = false
+        convertYamlToJava = false
+        precomputeOperations = true
+        cacheEnvironment = true
+        optimizeClassLoading = true
+        deduceEnvironment = true
+        optimizeNetty = true
+        replaceLogbackXml = true
+    }
+}
+
+
+publishing {
+
+    publications {
+        create<MavenPublication>("koolQueue") {
+            from(components["java"])
+            groupId = "joaquindiez"
+            artifactId = "micronaut-kool-queue-core"
+            version = "0.1.2"
+
+
+            pom {
+                name.set("micronaut-kool-queue-core")
+                description.set("Description of your package")
+                url.set("https://github.com/joaquindiez/micronaut-kool-queue")
+                licenses {
+                    license {
+                        name.set("Apache 2.0 License")
+                        url.set("https://opensource.org/license/apache-2-0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("joaquindiez")
+                        name.set("Joaquin Diez")
+                        email.set("joaquindiez@freesoullabs.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/joaquindiez/micronaut-kool-queue.git")
+                    developerConnection.set("scm:git:ssh://github.com:joaquindiez/micronaut-kool-queue.git")
+                    url.set("https://github.com/joaquindiez/micronaut-kool-queue")
+                }
+            }
+        }
+    }
+    repositories {
+       /* maven {
+            name = "localMaven"
+            url = uri("file:///Users/j10/.m2/repository/") // Define un directorio local para el repositorio
+        }*/
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/joaquindiez/kool-queue")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
---
+++ micronaut-kool-queue-core/src/main/kotlin/freesoullabs/koolQueue/controller/TaskController.kt
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2024 Joaquín Díez Gómez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package freesoullabs.koolQueue.controller
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import freesoullabs.koolQueue.domain.KoolQueueJobs
+import freesoullabs.koolQueue.services.KoolQueueJobsService
+
+
+@Controller("/v1/tasks")
+class TaskController(
+  private val taskService: KoolQueueJobsService, )  {
+
+
+  @Get
+  fun listTasks():HttpResponse<List<KoolQueueJobs>>{
+
+    return HttpResponse.ok(taskService.findAllTasks())
+  }
+
+}
